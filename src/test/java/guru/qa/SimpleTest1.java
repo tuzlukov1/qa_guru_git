05fa8d6cf9af22d09bd0b2a415e56f5225d53e36
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-03-01 01:24:58 +0700

    refactoring

--- src/test/java/guru/qa/SimpleTest1.java
+++ src/test/java/guru/qa/SimpleTest1.java
@@ -3,6 +3,7 @@ package guru.qa;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimpleTest1 {
 
@@ -10,4 +11,11 @@ public class SimpleTest1 {
     void assertTrueTest() {
         assertTrue(3 > 2);
     }
+
+    @Test
+    void assertFailedTest() {
+        fail("another fail");
+    }
+
+
 }
